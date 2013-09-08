5f4907619f2bcc60e93c2943eb606aac70a257b2
chengzhang <zc901218@gmail.com>
2013-09-08 07:15:50 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CourseraterApp::Application.config.secret_token = 'ae48cb59fffd3f4b152bfe4be7f9f1003139f1a1ddde9f026d29909565a3b03cc08a7c03dfa16667ca5d2de552d9b07da19a6470f193f2a44367893e735fc08c'
