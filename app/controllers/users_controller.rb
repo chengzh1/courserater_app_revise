b4ea5854cb53c4d45d2a9ceab847fce65cb54eab
chengzhang <zc901218@gmail.com>
2013-09-14 23:17:06 -0700

    Finish user eidt, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,51 @@
 class UsersController < ApplicationController
-before_filter :signed_in_user,
-    only: [:index, :edit, :update, :destroy, :following, :followers]
-  def new
-    @user = User.new
-  end
+    before_filter :signed_in_user,
+        only: [:index, :edit, :update, :destroy, :following, :followers]
+    before_filter :correct_user,   only: [:edit, :update]
+    before_filter :admin_user,     only: :destroy
     
-  def show
-    @user = User.find(params[:id])
-    @microposts = @user.microposts.paginate(page: params[:page])
-  end
-
+    def index
+        @users = User.paginate(page: params[:page])
+    end
+    
+    def new
+        @user = User.new
+    end
+    
+    def show
+        @user = User.find(params[:id])
+        @microposts = @user.microposts.paginate(page: params[:page])
+    end
+    
+    def edit
+        @user = User.find(params[:id])
+    end
+    
     def create
-        @user = User.new(params[:user])
-        if @user.save
-            sign_in @user  #定义在helper/sessions_helper
-            flash[:success] = "Welcome to CMU Course Rater!"
+        user = User.find_by_email(params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+            sign_in user
+            redirect_back_or user
+            else
+            flash.now[:error] = 'Invalid email/password combination'
+            render 'new'
+        end
+    end
+    
+    def destroy
+        User.find(params[:id]).destroy
+        flash[:success] = "User destroyed."
+        redirect_to users_url
+    end
+    
+    def update
+        @user = User.find(params[:id])
+        if @user.update_attributes(params[:user])
+            flash[:success] = "Profile updated"
+            sign_in @user
             redirect_to @user
             else
-            render 'new'
+            render 'edit'
         end
     end
     
@@ -33,6 +61,23 @@ before_filter :signed_in_user,
         @user = User.find(params[:id])
         @users = @user.followers.paginate(page: params[:page])
         render 'show_follow'
+    end
+    
+    private
+    def signed_in_user
+        unless signed_in?
+            store_location
+            redirect_to signin_url, notice: "Please sign in."
+        end
+    end
+    
+    def correct_user
+        @user = User.find(params[:id])
+        redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    def admin_user
+        redirect_to(root_url) unless current_user.admin?
     end
     
 end
